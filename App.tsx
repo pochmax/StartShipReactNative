b67f020361f01466cf35964a147cf0d6f9b9b90d
POCHET <max.pochet@orange.fr>
2022-02-22 13:48:57 +0100

    eslint + api key

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 // App.tsx
-
 import React from 'react';
 import LoginScreen from './src/screens/LoginScreen';
 import { StarshipFeedScreen } from './src/screens/StarshipFeedScreen';
@@ -19,3 +18,4 @@ const App = () => {
 };
 
 export default App;
+
